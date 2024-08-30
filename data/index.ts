6523d0b706d614443eecc321d7b0d28d1f950bf7
BeauEM <beau.d.elliott1@gmail.com>
2024-08-30 13:59:46 -0600

    Updating styles

--- data/index.ts
+++ data/index.ts
@@ -8,8 +8,9 @@ export const navItems = [
 export const gridItems = [
   {
     id: 1,
-    title: "I prioritize client collaboration, fostering open communication ",
-    description: "",
+    title:
+      "CRAFTING EXCEPTIONAL DIGITAL EXPERIENCES, FROM THE UI TO THE BACKEND",
+    description: "Doing things different...",
     className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
     imgClassName: "w-full h-full",
     titleClassName: "justify-end",
