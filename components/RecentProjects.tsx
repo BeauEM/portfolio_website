3e9197739797f0ae73d08efc9df1919cbe87f1d5
BeauEM <beau.d.elliott1@gmail.com>
2024-08-31 16:25:48 -0600

    Adding projects to work portfolio

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -1,35 +1,37 @@
 import { projects } from "@/data";
 import React from "react";
-import { div } from "three/webgpu";
 import { PinContainer } from "./ui/3d-pin";
 import { FaLocationArrow } from "react-icons/fa6";
 
 const RecentProjects = () => {
   return (
-    <div className="py-20">
+    <div className="py-20" id="projects">
       <h1 className="heading">
         A small selection of{" "}
         <span className="text-purple">recent projects</span>
       </h1>
-      <div className="flex flex-wrap items-center justify-center p-4 gap-16 mt-10">
+      <div className="flex flex-wrap items-center justify-center p-4 gap-x-24 gap-y-8 mt-10">
         {projects.map(({ id, title, des, img, iconLists, link }) => (
           <div
             key={id}
-            className="lg:min-h-[32.5rem] h-[25rem] flex items-center justify-center sm:w-96 w-[80vw]"
+            className="sm:h-[41rem] h-[32rem] lg:min-h-[32.5rem] flex items-center justify-center sm:w-[570px] w-[80vw]"
           >
             <PinContainer title={link} href={link}>
-              <div className="relative flex items-center justify-center sm:w-96 w-[80vw] h-[20vh] overflow-hidden lg:h-[30vh] mb-10">
+              <div className="relative flex items-center justify-center sm:w-[570px] sm:h-[40vh] w-[80vw] h-[30vh] overflow-hidden mb-10">
                 <div className="relative w-full h-full overflow-hidden lg:rounded-3xl bg-[#13162d]">
                   <img src="/bg.png" alt="bg-img" />
                 </div>
                 <img src={img} alt={title} className="z-10 absolute bottom-0" />
               </div>
+
               <h1 className="font-bold lg:text-2xl md:text-xl text-base line-clamp-1">
                 {title}
               </h1>
+
               <p className="lg:text-xl lg:font-normal font-light line-clamp-2">
                 {des}
               </p>
+
               <div className="flex items-center justify-between mt-7 mb-3">
                 <div className="flex items-center">
                   {iconLists.map((icon, index) => (
@@ -42,6 +44,7 @@ const RecentProjects = () => {
                     </div>
                   ))}
                 </div>
+
                 <div className="flex justify-center items-center">
                   <p className="flex lg:text-xl md:text-xs text-sm text-purple">
                     Check Live Site
