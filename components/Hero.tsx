01f7efa14719d0ba3ec4db0aa3a8076b3893044e
BeauEM <beau.d.elliott1@gmail.com>
2024-08-31 14:49:50 -0600

    Adding projects to home page

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -28,6 +28,7 @@ const Hero = () => {
           <h2 className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
             Dynamic Web Magic
           </h2>
+
           <TextGenerateEffect
             className="text-center text-[40px] md:text-5xl lg:text-6xl"
             words="Transforming Concepts into Seamless User Experiences"
